da4710de67020be316a0b58fc5efee6383984fdd
Sergei Terentev <sergei.terentev@aspose.com>
2023-10-17 08:46:18 +0300

    Update examples for v23.10

---
+++ Examples/GroupDocs.Conversion.Cloud.Examples.CSharp/Info/GetLicenseInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GroupDocs.Conversion.Cloud.Sdk.Api;
+
+namespace GroupDocs.Conversion.Cloud.Examples.CSharp.Info
+{
+    /// <summary>
+    /// This example demonstrates how to get metered license information
+    /// </summary>
+    public class GetLicenseInfo
+    {
+		public static void Run()
+		{
+            var apiInstance = new LicenseApi(Constants.GetConfig());
+
+			try
+			{
+				var response = apiInstance.GetLicenseInfo();
+
+                Console.WriteLine($"IsLicensed: {response.IsLicensed}");				
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception while calling LicenseApi: " + e.Message);
+			}
+		}
+	}
+}
